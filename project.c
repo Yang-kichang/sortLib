0a811f916827bb29b72138a14c64d5eeb860414e
abcdkh1209@soongsil.ac.kr <abcdkh120@soongsil.ac.kr>
2016-05-16 01:06:57 +0900

    make new branch

--- project.c
+++ project.c
@@ -1,40 +1,14 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
 
-int main(void)
+int main()
 {
-    char type,int_num[10][50],float_num[10][9],syntax[10];
-    int i,n;
-
-    i=0,n=0;
-
-    while((type=getchar())!='\n')
-    {
-        switch(type)
-        case '*':
-        case '%':
-        case '/':
-        case '+':
-        case '-':
-            syntax[i]=type;
-            i++;
-
-            break;
-
-        case '.':
-            float_num[i][n]=type;
-            n++;
-            break;
-
-        default:
-            int_num[i][n]=type;
-            break;
-    }
-
-        
-    
-    return 0;
+	char in;
+	char data[1000];
+	int p = 0;
+	while((in = getchar()) != '\n'){
+		if((in != ' '))
+			data[p++] = in;
+	}
+	puts(data);
+	return 0;
 }
-
-
-
